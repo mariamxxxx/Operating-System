f58d4d9736362a7b1c6af3742c58502f45e43b45
AiselWaleed <aiselwaleedd@gmail.com>
2026-04-12 13:16:38 +0200

    Altered parameters in mutex.c, mutex.h

--- src/synchronization/mutex.c
+++ src/synchronization/mutex.c
@@ -4,6 +4,125 @@
 #include "../scheduler/queue.h"
 #include "../scheduler/scheduler.h"
 
+int binSem [3]= {1,1,1};
+Queue blockedQueues[3];
+
+static const char* resource_name(enum RESOURCE r){
+    switch (r) {
+        case USER_INPUT:
+            return "USER_INPUT";
+        case USER_OUTPUT:
+            return "USER_OUTPUT";
+        case FILE_RESOURCE:
+            return "FILE_RESOURCE";
+        default:
+            return "INVALID_RESOURCE";
+    }
+}
+
+static const char* state_name(ProcessState state){
+    switch (state) {
+        case READY:
+            return "READY";
+        case RUNNING:
+            return "RUNNING";
+        case BLOCKED:
+            return "BLOCKED";
+        case FINISHED:
+            return "FINISHED";
+        default:
+            return "UNKNOWN_STATE";
+    }
+}
+
+static int queue_contains(Queue *q, Process *process){
+    QueueNode *current = q->head;
+
+    while (current != NULL) {
+        if (current->process == process) {
+            return 1;
+        }
+        current = current->next;
+    }
+
+    return 0;
+}
+
+void init_mutex(void){
+    int i;
+    for(i = 0; i < 3; i++){
+        binSem[i] = 1;
+        init_queue(&blockedQueues[i]);
+    }
+//COMMENT LATER
+    init_queue(&general_blocked_queue);
+    init_queue(&os_ready_queue);
+    //CHECK WITH ROUKA
+}
+
+void semWait(Process* process, enum RESOURCE r){
+    if (r < USER_INPUT || r > FILE_RESOURCE) {
+        printf("%s is invalid.\n", resource_name(r));
+        return;
+    }
+
+    if (queue_contains(&general_blocked_queue, process) || queue_contains(&blockedQueues[r], process)) {
+        process->pcb->state = BLOCKED;
+        printf("Process (pid = %d) is %s\n", process->pcb->pid, state_name(process->pcb->state));
+        return;
+    }
+
+    if(binSem[r]==1){
+        binSem[r]=0;
+    printf("%s is now occupied\n", resource_name(r));
+
+    }
+    else{
+        enqueue(process, &general_blocked_queue);
+        printf("Process (pid = %d) is enqueued in the general blocked queue.\n", process->pcb->pid);
+        enqueue(process, &blockedQueues[r]);
+        process->pcb->state= BLOCKED;
+        printf("Process (pid = %d) is enqueued in the %s blocked queue.\n", process->pcb->pid, resource_name(r));
+    }
+}
+
+void semSignal(enum RESOURCE r){
+    if (r < USER_INPUT || r > FILE_RESOURCE) {
+        printf("%s is invalid.\n", resource_name(r));
+        return;
+    }
+
+    if (is_empty(&blockedQueues[r])){
+        binSem[r]=1;
+    printf("%s is now available\n", resource_name(r));
+
+    }
+    else{
+        Process* nextProcess=dequeue(&blockedQueues[r]);
+        printf("Next Process (pid = %d) is dequeued from the %s blocked queue.\n", nextProcess->pcb->pid, resource_name(r));
+        int found=0;
+        int count=0;
+        while (!is_empty(&general_blocked_queue) && !found && count<general_blocked_queue.size){ //to avoid infinite loops
+            Process* val=dequeue(&general_blocked_queue);
+
+            if (val==nextProcess){
+                found=1;
+                printf("Next Process (pid = %d) is dequeued from the general blocked queue.\n", nextProcess->pcb->pid);
+            }
+            else
+                enqueue(val, &general_blocked_queue);
+            count++;
+        }
+        nextProcess->pcb->state = READY;
+        printf("Next Process (pid = %d) is %s\n", nextProcess->pcb->pid, state_name(nextProcess->pcb->state));
+        enqueue(nextProcess, &os_ready_queue);
+        printf("Next Process (pid = %d) is enqueued in the general ready queue.\n", nextProcess->pcb->pid);
+        binSem[r] = 0;
+    }
+}
+
+
+
 // void initQueue(CircularQueue *q){
 //     q-> front= -1;
 //     q-> rear= -1;
@@ -49,124 +168,7 @@
 //     return val;
 // }
 
-int binSem [3]= {1,1,1};
-Queue blockedQueues[3];
-
 
 // Queue blockedQueue;
 // Queue readyQueue;
 
-
-static const char* resource_name(enum RESOURCE r){
-    switch (r) {
-        case USER_INPUT:
-            return "USER_INPUT";
-        case USER_OUTPUT:
-            return "USER_OUTPUT";
-        case FILE_RESOURCE:
-            return "FILE_RESOURCE";
-        default:
-            return "INVALID_RESOURCE";
-    }
-}
-
-static const char* state_name(ProcessState state){
-    switch (state) {
-        case READY:
-            return "READY";
-        case RUNNING:
-            return "RUNNING";
-        case BLOCKED:
-            return "BLOCKED";
-        case FINISHED:
-            return "FINISHED";
-        default:
-            return "UNKNOWN_STATE";
-    }
-}
-
-static int queue_contains(Queue *q, PCB *process){
-    QueueNode *current = q->head;
-
-    while (current != NULL) {
-        if (current->process == process) {
-            return 1;
-        }
-        current = current->next;
-    }
-
-    return 0;
-}
-
-void init_mutex(void){
-    int i;
-    for(i = 0; i < 3; i++){
-        binSem[i] = 1;
-        init_queue(&blockedQueues[i]);
-    }
-//COMMENT LATER
-    init_queue(&general_blocked_queue);
-    init_queue(&os_ready_queue);
-    //CHECK WITH ROUKA
-}
-
-void semWait(PCB* process, enum RESOURCE r){
-    if (r < USER_INPUT || r > FILE_RESOURCE) {
-        printf("%s is invalid.\n", resource_name(r));
-        return;
-    }
-
-    if (queue_contains(&general_blocked_queue, process) || queue_contains(&blockedQueues[r], process)) {
-        process->state = BLOCKED;
-        printf("Process (pid = %d) is %s\n", process->pid, state_name(process->state));
-        return;
-    }
-
-    if(binSem[r]==1){
-        binSem[r]=0;
-    printf("%s is now occupied\n", resource_name(r));
-
-    }
-    else{
-        enqueue(process, &general_blocked_queue);
-        printf("Process (pid = %d) is enqueued in the general blocked queue.\n", process -> pid);
-        enqueue(process, &blockedQueues[r]);
-        process -> state = BLOCKED;
-        printf("Process (pid = %d) is enqueued in the %s blocked queue.\n", process->pid, resource_name(r));
-    }
-}
-
-void semSignal(enum RESOURCE r){
-    if (r < USER_INPUT || r > FILE_RESOURCE) {
-        printf("%s is invalid.\n", resource_name(r));
-        return;
-    }
-
-    if (is_empty(&blockedQueues[r])){
-        binSem[r]=1;
-    printf("%s is now available\n", resource_name(r));
-
-    }
-    else{
-        PCB* nextProcess=dequeue(&blockedQueues[r]);
-        printf("Next Process (pid = %d) is dequeued from the %s blocked queue.\n", nextProcess->pid, resource_name(r));
-        int found=0;
-        int count=0;
-        while (!is_empty(&general_blocked_queue) && !found && count<general_blocked_queue.size){ //to avoid infinite loops
-            PCB* val=dequeue(&general_blocked_queue);
-
-            if (val==nextProcess){
-                found=1;
-                printf("Next Process (pid = %d) is dequeued from the general blocked queue.\n", nextProcess -> pid);
-            }
-            else
-                enqueue(val, &general_blocked_queue);
-            count++;
-        }
-        nextProcess->state = READY;
-        printf("Next Process (pid = %d) is %s\n", nextProcess->pid, state_name(nextProcess->state));
-        enqueue(nextProcess, &os_ready_queue);
-        printf("Next Process (pid = %d) is enqueued in the general ready queue.\n", nextProcess -> pid);
-        binSem[r] = 0;
-    }
-}
